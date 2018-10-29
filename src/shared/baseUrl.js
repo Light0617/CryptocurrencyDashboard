0f031f009fb09d9caf1cd0acc37ac8223d624f1b
light0617 <light11470617@gmail.com>
2018-10-28 17:02:39 -0700

    for local

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,2 @@
-//export const baseUrl = 'http://localhost:3003/';
-export const baseUrl = 'http://52.13.216.5:3003/';
+export const baseUrl = 'http://localhost:3003/';
+//export const baseUrl = 'http://52.13.216.5:3003/';
