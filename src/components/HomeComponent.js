3211c99b0dd9d0eafbe20107b785311a96bc8efd
light0617 <light11470617@gmail.com>
2018-10-28 19:34:35 -0700

    update home

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -7,16 +7,24 @@ function Home(props) {
       <div className="row"> <h1>Home</h1></div>
       <div className="row"> <h3>If you do not want to sign up, please login with (guest/123)</h3></div>
       <div className="row"> 
-        <div className="col-sm-10">
+        <div className="col-sm-5">
           <Card style={{ backgroundColor: '#aaaaaa' }}>
             <CardTitle>Dashboard</CardTitle>
             <img src='demo1.png' alt='dashboard' height='500px'/>
           </Card>
         </div>
-        <div className="col-sm-10">
+        <div className="col-sm-5">
           <Card style={{ backgroundColor: '#aaaaaa' }}>
             <CardTitle>Coins</CardTitle>
             <img src='demo2.png' alt='coins' height='500px'/>
+          </Card>
+        </div>
+      </div>
+      <div className="row"> 
+        <div className="col-sm-10">
+          <Card style={{ backgroundColor: '#aaaaaa' }}>
+            <CardTitle>DataFlow</CardTitle>
+            <img src='demo1.png' alt='dashboard' height='500px'/>
           </Card>
         </div>
       </div>
